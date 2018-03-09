219122586dbc574bf9c0577edce00c10c5d12905
mwlai288 <mwlai288@gmail.com>
2018-03-09 17:52:19 -0500

    added expect assertion library

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,3 +1,5 @@
+import expect from 'expect';
+
 const add = (a, b) => {
   if (typeof b !== 'number') {
     return a + a;
@@ -10,17 +12,14 @@ const square = (a) => a * a;
 describe('add', function() {
   it('should add two numbers', function() {
     const total = add(4, 2);
-    if (total !== 6) {
-      throw new Error('Your math sucks');
-    }
+
+    expect(total).toBe(6);
   });
 
   it('should double a single number', function() {
     const res = add(44);
 
-    if (res !== 88) {
-      throw new Error('Check your math fool');
-    }
+    expect(res).toBe(88);
   });
 });
 
@@ -28,8 +27,6 @@ describe('square', function() {
   it('should square a number', function() {
     const squared = square(4);
 
-    if (squared !== 16) {
-      throw new Error('Boy you dumb');
-    }
+    expect(squared).toBe(16);
   });
 });
